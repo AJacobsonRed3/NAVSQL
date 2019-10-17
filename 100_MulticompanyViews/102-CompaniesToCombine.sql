4815c97adb9681defa10640d1f7e9b5be53cae5a
Adam Jacobson <adam@redthree.com>
2019-10-17 12:52:41 -0400

    Update 102-CompaniesToCombine.sql

--- 100_MulticompanyViews/102-CompaniesToCombine.sql
+++ 100_MulticompanyViews/102-CompaniesToCombine.sql
@@ -1,6 +1,7 @@
 /*I don't assume that you will want to combine all companies.
   In addition, while we could use the name that NAv uses for each of your companies in the query, sometimes it's easier to create
   an abbreviation.  
+  Please make sure you have your reporting database setup for the desired NAV Database you want to report over.
  */
 USE ReportingDatabase -- Replace This with your reporting database name
 GO
@@ -29,4 +30,21 @@ SQLCompanyPrefix - the prefix uses for a company's data (special Characters remo
 Abbr - an abbreviation to store in the view. For example, I use CAN for Canada
 Name - the name of the company.  (not required in this context, used for other reporting example that I may post on Github)
 Default Company - Y/N - (not required in this context, used for other reporting example that I may post on Github)
+
+The following insert Command will populate values for a system that had three databases, each storing the three Cronus Companies
+with three matching reporting databases
 */
+INSERT INTO dbo.Companies
+VALUES
+( 'ReportingDev', 'NAVCronusDev', N'CRONUS Canada, Inc_', N'CAN', N'Cronus Canada', ' ' ), 
+( 'ReportingDev', 'NAVCronusDev', N'CRONUS Mexico S_A_', N'MEX', N'Cronus Mexico', ' ' ), 
+( 'ReportingDev', 'NAVCronusDev', N'CRONUS USA, Inc_', N'USA', N'Cronus USA', 'Y' ), 
+( 'ReportingProd', 'NAVCronusProd', N'CRONUS Canada, Inc_', N'CAN', N'Cronus Canada', ' ' ), 
+( 'ReportingProd', 'NAVCronusProd', N'CRONUS Mexico S_A_', N'MEX', N'Cronus Mexico', ' ' ), 
+( 'ReportingProd', 'NAVCronusProd', N'CRONUS USA, Inc_', N'USA', N'Cronus USA', 'Y' ), 
+( 'ReportingTest', 'NAVCronusTest', N'CRONUS Canada, Inc_', N'CAN', N'Cronus Canada', ' ' ), 
+( 'ReportingTest', 'NAVCronusTest', N'CRONUS Mexico S_A_', N'MEX', N'Cronus Mexico', ' ' ), 
+( 'ReportingTest', 'NAVCronusTest', N'CRONUS USA, Inc_', N'USA', N'Cronus USA', 'Y' )
+
+
+
